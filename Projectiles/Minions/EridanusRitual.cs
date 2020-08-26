a7a01f8fb0bd9c7dc1e4c11ecdbd7a884ab5686c
Blockaroz <lbpk.123fan@gmail.com>
2020-08-26 02:11:41 -0400

    Revert "eridanus glowup #1"
    This reverts commit 875934ee9939cfad18e75d68180620d07468f2f3.

--- Projectiles/Minions/EridanusRitual.cs
+++ Projectiles/Minions/EridanusRitual.cs
@@ -77,8 +77,6 @@ namespace FargowiltasSouls.Projectiles.Minions
 
         public override bool PreDraw(SpriteBatch spriteBatch, Color lightColor)
         {
-            spriteBatch.End();
-            spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.Additive, Main.DefaultSamplerState, DepthStencilState.None, RasterizerState.CullCounterClockwise, null, Main.Transform);
             Texture2D texture2D13 = Main.projectileTexture[projectile.type];
             int num156 = Main.projectileTexture[projectile.type].Height / Main.projFrames[projectile.type]; //ypos of lower right corner of sprite to draw
             int y3 = num156 * projectile.frame; //ypos of upper left corner of sprite to draw
@@ -101,7 +99,7 @@ namespace FargowiltasSouls.Projectiles.Minions
 
         public override Color? GetAlpha(Color lightColor)
         {
-            return Color.White * projectile.Opacity;
+            return Color.White * projectile.Opacity * .75f;
         }
     }
 }
